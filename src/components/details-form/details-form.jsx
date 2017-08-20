e7e5bb6907c488c00839a8273d3f498092913c6d
RMHonor <me@r-honor.uk>
2017-08-20 21:05:18 +0100

    Button component added, text input moved

--- src/components/details-form/details-form.jsx
+++ src/components/details-form/details-form.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 
-import Input from '../common/input/input';
+import Input from '../common/input/text/input';
 
 const fields = {
   salary: {
