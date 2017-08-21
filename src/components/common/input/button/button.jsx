ea64533822c255fe5777ba4dc4cda22cb00df999
RMHonor <me@r-honor.uk>
2017-08-21 17:33:56 +0100

    Spacing added to button

--- src/components/common/input/button/button.jsx
+++ src/components/common/input/button/button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './button.scss';
 
 export default props => (
-  <div className="button__container">
+  <div className="button__container margin--top--20">
     <button onClick={props.handleSubmit} className="button">{props.label}</button>
   </div>
 );
