8e2e07191354e03e903a51f0bf4113eab2ed22ca
RMHonor <me@r-honor.uk>
2017-09-01 18:15:28 +0100

    Added calculator functions

--- src/actions/calculator.js
+++ src/actions/calculator.js
@@ -11,3 +11,22 @@ export default function (details) {
 export function getMonthlyPayment(salary, threshold) {
   return Math.max((salary - threshold) * 0.09, 0);
 }
+
+export function getInterest(salary, lowerThreshold, upperThreshold, inflation) {
+  const thresholdRange = upperThreshold - lowerThreshold;
+  const salaryAboveThreshold = salary - lowerThreshold;
+
+  const percentIntoThresholdRange = Math.max(0, Math.min(salaryAboveThreshold / thresholdRange, 1));
+
+  return (percentIntoThresholdRange * 3) + inflation;
+}
+
+export function calculateMonthBalanceChange(balance, payment, interest) {
+  const postPaymentBalance = balance - payment;
+
+  if (postPaymentBalance < 0) {
+    return postPaymentBalance;
+  }
+
+  return postPaymentBalance * (1 + ((interest / 12) / 100));
+}
--- src/actions/calculator.spec.js
+++ src/actions/calculator.spec.js
@@ -1,11 +1,13 @@
 import {
   getMonthlyPayment,
+  getInterest,
+  calculateMonthBalanceChange,
 } from './calculator';
 
 /*eslint-disable no-undef*/
 
-describe('Loan Calculator - ', () => {
-  describe('getMonthlyPayment - ', () => {
+describe('Loan Calculator', () => {
+  describe('getMonthlyPayment', () => {
     it('should return 0 if salary below threshold', () => {
       const payment = getMonthlyPayment(1000, 1200);
 
@@ -22,6 +24,71 @@ describe('Loan Calculator - ', () => {
       const payment = getMonthlyPayment(1100, 1000);
 
       expect(payment).to.equal(9);
+    });
+  });
+
+  describe('getSalaryDependentInterestRate', () => {
+    it('should return 3 if salary below lower threshold and inflation is 3', () => {
+      const interest = getInterest(0, 1, 2, 3);
+
+      expect(interest).to.equal(3);
+    });
+
+    it('should return 3 if salary equal to lower threshold and inflation is 3', () => {
+      const interest = getInterest(1, 1, 2, 3);
+
+      expect(interest).to.equal(3);
+    });
+
+    it('should return 1.5 if salary exactly in between thresholds and inflation is 0', () => {
+      const interest = getInterest(5, 0, 10, 0);
+
+      expect(interest).to.equal(1.5);
+    });
+
+    it('should return 3 if salary above upper threshold and inflation is 0', () => {
+      const interest = getInterest(10, 0, 5, 0);
+
+      expect(interest).to.equal(3);
+    });
+
+    it('should return 3 if salary equal to upper threshold and inflation is 0', () => {
+      const interest = getInterest(5, 0, 5, 0);
+
+      expect(interest).to.equal(3);
+    });
+  });
+
+  describe('calculateMonthBalanceChange', () => {
+    it('should return the same balance if payment and interest are 0', () => {
+      const balance = calculateMonthBalanceChange(1000, 0, 0);
+
+      expect(balance).to.equal(1000);
+    });
+
+    it('should return higher balance if no payment and interest applied', () => {
+      const balance = calculateMonthBalanceChange(1000, 0, 12);
+
+      expect(balance).to.equal(1010);
+    });
+
+    it('should return a 100 reduction is payment is 100 and interest is 0', () => {
+      const balance = calculateMonthBalanceChange(1000, 100, 0);
+
+      expect(balance).to.equal(900);
+    });
+
+    it('should apply payment before interest', () => {
+      const balance = calculateMonthBalanceChange(1100, 100, 12);
+
+      expect(balance).to.equal(1010);
+      expect(balance).to.not.equal(1011);
+    });
+
+    it('should return negative value if payment higher than balance', () => {
+      const balance = calculateMonthBalanceChange(100, 200, 0);
+
+      expect(balance).to.equal(-100);
     });
   });
 });
