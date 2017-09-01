d9ef2667ef5507212c3c2ebbea9aa27169bcbdbf
RMHonor <me@r-honor.uk>
2017-09-01 18:16:20 +0100

    Reorder functions

--- src/actions/calculator.js
+++ src/actions/calculator.js
@@ -8,10 +8,6 @@ export default function (details) {
   };
 }
 
-export function getMonthlyPayment(salary, threshold) {
-  return Math.max((salary - threshold) * 0.09, 0);
-}
-
 export function getInterest(salary, lowerThreshold, upperThreshold, inflation) {
   const thresholdRange = upperThreshold - lowerThreshold;
   const salaryAboveThreshold = salary - lowerThreshold;
@@ -19,6 +15,10 @@ export function getInterest(salary, lowerThreshold, upperThreshold, inflation) {
   const percentIntoThresholdRange = Math.max(0, Math.min(salaryAboveThreshold / thresholdRange, 1));
 
   return (percentIntoThresholdRange * 3) + inflation;
+}
+
+export function getMonthlyPayment(salary, threshold) {
+  return Math.max((salary - threshold) * 0.09, 0);
 }
 
 export function calculateMonthBalanceChange(balance, payment, interest) {
