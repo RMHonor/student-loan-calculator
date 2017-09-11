2cb5848009f8de1c9ff1b6ed66dec2f08bde5039
RMHonor <me@r-honor.uk>
2017-09-11 21:20:28 +0100

    Minor form changes

--- src/components/results-table/results-table.jsx
+++ src/components/results-table/results-table.jsx
@@ -3,13 +3,10 @@ import { connect } from 'react-redux';
 
 
 function ResultsTable(props) {
-  if (!props.data) {
-    return null;
-  }
+  if (!props.data) return null;
 
-  console.log(props.data);
   return (
-    <table>
+    <table className="results-table">
       <thead>
         <tr>
           <th>Year</th>
@@ -23,13 +20,22 @@ function ResultsTable(props) {
         {props.data.map(year => (
           <tr key={year.years}>
             <td>{year.years}</td>
-            <td>{Math.round(year.endingBalance)}</td>
-            <td>{Math.round(year.salary)}</td>
-            <td>{Math.round(year.paid)}</td>
-            <td>{Math.round(year.interest)}</td>
+            <td>£{Math.round(year.endingBalance)}</td>
+            <td>£{Math.round(year.salary)}</td>
+            <td>£{Math.round(year.paid)}</td>
+            <td>£{Math.round(year.interest)}</td>
           </tr>
         ))}
       </tbody>
+      <tfoot>
+        <tr>
+          <th>Year</th>
+          <th>Balance</th>
+          <th>Salary</th>
+          <th>Paid</th>
+          <th>Interest</th>
+        </tr>
+      </tfoot>
     </table>
   );
 }
