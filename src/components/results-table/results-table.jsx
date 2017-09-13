7f08ce8549fd033033f69d7dd64c3e15ff1dbf60
RMHonor <me@r-honor.uk>
2017-09-13 08:34:58 +0100

    Added styling for table

--- src/components/results-table/results-table.jsx
+++ src/components/results-table/results-table.jsx
@@ -1,42 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import './results-table.scss';
 
 function ResultsTable(props) {
   if (!props.data) return null;
 
   return (
-    <table className="results-table">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Balance</th>
-          <th>Salary</th>
-          <th>Paid</th>
-          <th>Interest</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.data.map(year => (
-          <tr key={year.years}>
-            <td>{year.years}</td>
-            <td>£{Math.round(year.endingBalance)}</td>
-            <td>£{Math.round(year.salary)}</td>
-            <td>£{Math.round(year.paid)}</td>
-            <td>£{Math.round(year.interest)}</td>
+    <div className="margin--bottom--20">
+      <table className="results-table">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Balance</th>
+            <th>Salary</th>
+            <th>Paid</th>
+            <th>Interest</th>
           </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        <tr>
-          <th>Year</th>
-          <th>Balance</th>
-          <th>Salary</th>
-          <th>Paid</th>
-          <th>Interest</th>
-        </tr>
-      </tfoot>
-    </table>
+        </thead>
+        <tbody>
+          {props.data.map(year => (
+            <tr key={year.years}>
+              <td>{year.years}</td>
+              <td>£{Math.round(year.endingBalance)}</td>
+              <td>£{Math.round(year.salary)}</td>
+              <td>£{Math.round(year.paid)}</td>
+              <td>£{Math.round(year.interest)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
