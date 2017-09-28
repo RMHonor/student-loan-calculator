c8477d8d89ab1aea617606622d0fd59273a13763
RMHonor <me@r-honor.uk>
2017-09-28 18:23:57 +0100

    Reducers to get max balance through loan term

--- src/components/results-graph/paint-graph.js
+++ src/components/results-graph/paint-graph.js
@@ -1,3 +1,16 @@
-export default function(node) {
-  console.log(node);
+import { select } from 'd3-selection';
+
+export default function (node, data) {
+  //TODO streamline this
+  const maxBalance = data.reduce((maxBalanceAcc, { months }) => (
+    Math.max(
+      maxBalanceAcc,
+      months.reduce(
+        (maxBalanceInYear, month) => Math.max(maxBalanceInYear, month.balance),
+        0,
+      ),
+    )
+  ), 0);
+
+  select(node);
 }
