3d2641332a9bff15f722e4414a73b65f83f31fc2
Richard Honor <me@r-honor.uk>
2017-10-29 16:24:11 +0000

    Removed eslint override

--- src/reducers/calculator.js
+++ src/reducers/calculator.js
@@ -1,6 +1,5 @@
 import { CALCULATE_LOAN } from '../actions/calculator/action';
 
-/* eslint-disable max-len */
 export default function (state = null, action) {
   switch (action.type) {
     case CALCULATE_LOAN:
